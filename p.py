beab786c6f3fe80c6d5ffd00bb08efd42ff8d747
netrc <ric@netrc.com>
2016-08-05 20:09:01 -0400

    modularize, hide main

--- p.py
+++ p.py
@@ -1,26 +1,41 @@
+#!/usr/bin/env python
 
 import os
 import sys
 import logging
 
-# just supports stdin, and set of option letters 
-pname = sys.argv[0]
 
-debug=0
+# can define funcs for module "p"
+def setLog(l):
+	logging.basicConfig(level=l)
 
-if (len(sys.argv)>1):
-		for c in sys.argv[1]:
-			if (c == 'd'):
-				debug=1
-				logging.basicConfig(level=logging.DEBUG)
-			elif (c=='n'):
-				noop=1
-			elif (c=='-'):
-				pass # ignore
-			else:
-				logging.warn("bad option: %c", c)
 
-# or if (len(sys.argv)==3):
-#		fname=sys.argv[2]
 
-logging.debug("starting: %s", pname)
+# Main
+def main():
+	# whatever
+	logging.debug("main")
+
+
+if __name__ == '__main__':
+	# just supports stdin, and set of option letters 
+	pname = sys.argv[0]
+
+	if (len(sys.argv)>1):
+			for c in sys.argv[1]:
+				if (c == 'd'):
+					debug=1
+					setLog(logging.DEBUG)
+				elif (c=='n'):
+					noop=1
+				elif (c=='-'):
+					pass # ignore
+				else:
+					logging.warn("bad option: %c", c)
+
+	# or if (len(sys.argv)==3):
+	#		fname=sys.argv[2]
+
+	logging.debug("starting: %s", pname)
+	main()
+	sys.exit(0)
