a50226d3edc6eadc5ef6f41e5fc59e92ba4427be
netrc <ric@netrc.com>
2016-08-06 15:18:32 -0400

    with unittests

--- p.py
+++ p.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import logging
+import unittest
 
 
 # can define funcs for module "p"
@@ -19,7 +20,17 @@ class PClass(object):
 	def __str__(self):
 		return "PClass(" + str(self.x) + ")"
 
+	def value(self):
+		return self.x
 
+# Test Code
+class PUnitTest(unittest.TestCase):
+	def setUp(self):
+		# can do one time setup here
+		self.p = PClass(2)
+
+	def test_obj_is_ok(self):
+		self.assertEqual( 2, self.p.value() )
 
 # Main
 def main():
@@ -33,21 +44,27 @@ if __name__ == '__main__':
 	# just supports stdin, and set of option letters 
 	pname = sys.argv[0]
 
+	runFunc = main	
 	if (len(sys.argv)>1):
 			for c in sys.argv[1]:
 				if (c == 'd'):
 					debug=1
 					setLog(logging.DEBUG)
-				elif (c=='n'):
-					noop=1
+				elif (c=='t'):
+					runFunc = unittest.main
+					sys.argv.remove(sys.argv[1]) # add unittest args after 
+					break
 				elif (c=='-'):
 					pass # ignore
 				else:
-					logging.warn("bad option: %c", c)
+					logging.error("bad option: %c", c)
+					print("usage: [-d]   - debug")
+					print("usage: [-t  [unittest args]  - test")
+					sys.exit(-1)
 
 	# or if (len(sys.argv)==3):
 	#		fname=sys.argv[2]
 
 	logging.debug("starting: %s", pname)
-	main()
+	runFunc()
 	sys.exit(0)
